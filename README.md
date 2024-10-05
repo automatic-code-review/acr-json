d0a38c2e18d38bcbeb2aac454d5815d511735adc
kielsonzinn <kielsonzinn@gmail.com>
2024-10-05 15:12:52 -0300

    feat: Cria regra da extensão

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-# acr-extension-name
+# acr-json
 
 Arquivo config.json
 
 ```json
 {
-
+  "stage": "static",
+  "language": "python",
+  "messageError": "JSON incorreto no arquivo ${FILE_PATH}",
+  "messageIdent": "JSON com identação incorreta no arquivo ${FILE_PATH}\n```json\n${JSON_FORMATTED}\n```"
 }
 ```
--- src/review.py
+++ src/review.py
@@ -1,25 +1,84 @@
+import hashlib
+import json
+import os
+from json import JSONDecodeError
+
 import automatic_code_review_commons as commons
 
-def review(config):
-    path_target = config['path_target']
-    path_source = config['path_source']
 
-    merge = config['merge']
-    project_id = merge['project_id']
-    merge_request_id = merge['merge_request_id']
-    
+def generate_md5(path):
+    hash_md5 = hashlib.md5()
+
+    try:
+        with open(path, 'rb') as file:
+            for chunk in iter(lambda: file.read(4096), b""):
+                hash_md5.update(chunk)
+
+        return hash_md5.hexdigest()
+    except FileNotFoundError as _:
+        return None
+
+
+def review(config):
+    path_source = config['path_source']
+    changes = config['merge']['changes']
+    message_error = config['messageError']
+    message_ident = config['messageIdent']
+
     comments = []
-    
-    # TODO IMPLEMENTAR EXTENSION
-    #  O OBJETO DE COMENTARIO DEVE POSSUIR O SEGUINTE FORMATO
-    #  commons.comment_create(
-    #     comment_id=commons.comment_generate_id( "" ),
-    #     comment_path="",
-    #     comment_description="",
-    #     comment_snipset=True,
-    #     comment_end_line=1,
-    #     comment_start_line=1,
-    #     comment_language="",
-    # )
+
+    for change in changes:
+        new_path = change['new_path']
+
+        if not new_path.endswith(".json"):
+            continue
+
+        path = os.path.join(path_source, new_path)
+
+        try:
+            with open(path, 'r') as content_data:
+                data = json.load(content_data)
+
+        except JSONDecodeError as _:
+
+            comment_path = new_path
+            comment_description = message_error
+            comment_description = comment_description.replace("${FILE_PATH}", comment_path)
+
+            print(comment_description)
+
+            comments.append(commons.comment_create(
+                comment_id=commons.comment_generate_id(comment_path),
+                comment_path=comment_path,
+                comment_description=comment_description,
+                comment_snipset=False,
+                comment_end_line=1,
+                comment_start_line=1,
+                comment_language="json",
+            ))
+        else:
+            path_tmp = "/tmp/acr_json.json"
+            with open(path_tmp, 'w') as file:
+                json.dump(data, file, indent=4, ensure_ascii=False)
+
+            if generate_md5(path) != generate_md5(path_tmp):
+                json_formatted = json.dumps(data, indent=4, ensure_ascii=False)
+
+                comment_path = new_path
+                comment_description = message_ident
+                comment_description = comment_description.replace("${FILE_PATH}", comment_path)
+                comment_description = comment_description.replace("${JSON_FORMATTED}", json_formatted)
+
+                print(comment_description)
+
+                comments.append(commons.comment_create(
+                    comment_id=commons.comment_generate_id(comment_path),
+                    comment_path=comment_path,
+                    comment_description=comment_description,
+                    comment_snipset=False,
+                    comment_end_line=1,
+                    comment_start_line=1,
+                    comment_language="json",
+                ))
 
     return comments
